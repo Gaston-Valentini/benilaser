5b926ef3e2fadc99a941fbc09c71feddd6332fd1
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-03-26 13:06:56 +0100

    Add: About section added.

---
+++ src/sections/About/About.jsx
@@ -0,0 +1,29 @@
+import style from "./About.module.css";
+import { FaInstagram, FaFacebookF } from "react-icons/fa";
+
+export default function About() {
+    return (
+        <section className={style.container} id="about">
+            <div className={style.data}>
+                <div className={style.dataImage}>
+                    <img src="https://www.latevaweb.com/diseno-web/clinica-dental.jpg" alt="profilePhoto" />
+                </div>
+                <div className={style.dataInfo}>
+                    <div className={style.dataInfoTitle}>¿Quiénes somos?</div>
+                    <div className={style.dataInfoBio}>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, aut distinctio quo ipsum itaque vel rerum ut ducimus sapiente esse quaerat ratione assumenda, excepturi libero vero culpa a repellat. Asperiores. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, aut distinctio quo ipsum itaque vel rerum ut ducimus sapiente esse quaerat ratione assumenda, excepturi libero vero culpa a repellat. Asperiores. Lorem ipsum dolor, sit amet consectetur
+                        adipisicing elit. Repudiandae, aut distinctio quo ipsum itaque vel rerum ut ducimus sapiente esse quaerat ratione assumenda, excepturi libero vero culpa a repellat. Asperiores.
+                    </div>
+                    <div className={style.dataInfoButtons}>
+                        <a href="https://github.com/Gaston-Valentini" target="_blank" rel="noopener noreferrer">
+                            <FaInstagram className={style.dataInfoButtonsButton} />
+                        </a>
+                        <a href="https://www.linkedin.com/in/gastonvalentini/" target="_blank" rel="noopener noreferrer">
+                            <FaFacebookF className={style.dataInfoButtonsButton} />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,12 @@
 import style from "./Home.module.css";
 import HomeHeader from "../../sections/HomeHeader/HomeHeader";
+import About from "../../sections/About/About";
 
 export default function Home() {
     return (
         <section className={style.container}>
             <HomeHeader />
+            <About />
         </section>
     );
 }
