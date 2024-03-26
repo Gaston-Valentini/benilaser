9b1729ca955ed397a889678cd524243874de38cc
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-03-26 16:18:00 +0100

    Add: Hook section added.

---
+++ src/sections/Hook/Hook.jsx
@@ -0,0 +1,20 @@
+import style from "./Hook.module.css";
+import { FaRegCalendarCheck } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Hook() {
+    return (
+        <div className={style.container}>
+            <div className={style.title}>Consulta gratuita</div>
+            <div className={style.icon}>
+                <hr />
+                <FaRegCalendarCheck />
+                <hr />
+            </div>
+            <div className={style.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum deleniti laudantium, deserunt rem at excepturi, dignissimos tenetur labore magnam nihil assumenda dolore pariatur nemo velit aut tempora laborum fuga libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores accusamus quo enim, doloribus ullam fuga consequatur laborum! Iure ipsam consequuntur, a natus nobis excepturi quia delectus praesentium harum quo debitis.</div>
+            <Link to="/contact" className={style.button}>
+                CONTACTANOS
+            </Link>
+        </div>
+    );
+}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,14 @@
 import style from "./Home.module.css";
 import HomeHeader from "../../sections/HomeHeader/HomeHeader";
 import About from "../../sections/About/About";
+import Hook from "../../sections/Hook/Hook";
 
 export default function Home() {
     return (
         <section className={style.container}>
             <HomeHeader />
             <About />
+            <Hook />
         </section>
     );
 }
